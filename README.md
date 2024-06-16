76ee9aed99245252f611c1cf6e1e3d78d64bd3b1
Zhenggang Tang <tangzhenggang@pku.edu.cn>
2024-06-15 23:33:46 -0700

    init

--- README.md
+++ README.md
@@ -10,11 +10,22 @@ we put our full new view synthesis and geometry results in [our_evaluations](htt
 
 ## code
 
-The code is separated to three parts: dense **correspondence matching**, **embedded deformation graph** (EDG) optimization, **NeRF new view synthesis** in transformed scene using EDG as deformation field.
+The code is separated to three parts: dense correspondence **matching**, correspondence **filtering**, **embedded deformation graph** (EDG) optimization, NeRF **new view synthesis in transformed scene** using EDG as deformation field. We have a data_sample for inference
 
 They are located in [src/correspondence_matching](), [src/EDG]() and [src/NeRF]().
 
 ### correspondence matching
+
+Our corrspondence matching code is based on [ASpanFormer](https://github.com/apple/ml-aspanformer). We also use its outdoor pretrained model for inference. We include the pretrained model in the repository. 
+
+To install the conda virtual environment, run `conda install --file src/correspondence_matching/environment.yaml`.
+
+Then run the following commands to do matching on the sample data.
+
+```python
+cd src/correspondence_matching/demo
+python match.py --img0_path "../../data_sample/transformed_view.png" --img1_path ../../data_sample/original_views/ --out_path ../../matching_output/ --long_dim0 800 --long_dim1 800
+```
 
 #### filtering
 
@@ -22,6 +33,13 @@ They are located in [src/correspondence_matching](), [src/EDG]() and [src/NeRF](
 
 ### NeRF
 
-the full code for training the original NeRF and rendering from the transformed NeRF is coming soon. We are 
+Again, just run `conda install --file src/NeRF/environment.yaml` to install the conda virtual environment.
+
+Here we provide the code that defines the forward flow to bend rays for NeRF rendering in the transformed scene. The full code for training the original NeRF and rendering from the transformed NeRF is coming soon. 
 
 Our code is based on [ASpanFormer](https://github.com/apple/ml-aspanformer), [Nonrigid-ICP](https://github.com/rabbityl/Nonrigid-ICP-Pytorch) and [NeRFStudio](https://github.com/nerfstudio-project/nerfstudio) respectively. Thanks a lot to these great codebases!
+
+
+CUDA_VISIBLE_DEVICES=7 python demo.py --img0_path "../data_nerf/cmc-render-main/dra_17/000009.png" --img1_path ../renders_high/dra_20_original_high/ --out_path /data01/zt15/outputs/aspan_outputs/output/dra_20_17_9_nerf_high --long_dim0 800 --long_dim1 800
+
+CUDA_VISIBLE_DEVICES=7 python demo.py --img0_path "../data_sample/transformed_view/000009.png" --img1_path ../data_sample/original_views/ --out_path ../matching_output/ --long_dim0 800 --long_dim1 800
--- src/NeRF/deformation_field_separated.py
+++ src/NeRF/deformation_field_separated.py
@@ -2,34 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
+from pytorch3d import ops
 
-from nerfstudio.fields.geom_utils import *
 
+def argmin_dis_batch(p, q, K=1):
+    # print("argmin_dis_batch (pytorch3d)", p.shape, q.shape) # [N, 3], [M, 3]
 
-class Deformation(nn.Module):
-    def __init__(self, body_config: dict, embedding_config: dict):
-        super(Deformation, self).__init__()
-        self.embedding, self.input_size, self.input_dim, self.n_freq = get_embedder(**embedding_config)
-        body_config["input_ch"] = self.input_size
-        body_config["input_dim"] = self.input_dim
-        body_config["n_freq"] = self.n_freq
-        self.body = body_config["type"](**body_config)
+    result = ops.knn_points(p[None], q[None], K=K)
 
-        self.train_step = None
+    return result[0][0, :, :], result[1][0, :, :]
 
-    def forward(self, x):
-        original_shape = x.shape
-        res = self.forward2D(x.reshape(-1, x.shape[-1]))
-        res[0] = res[0].reshape(*original_shape[:-1], -1)
-        return res
+def Rx(R, x):
+    # print("Rx", R.shape, x.shape)
+    return (R * x.unsqueeze(-2)).sum(-1)
 
-    def forward2D(self, x):
-        deform_rot = None
-        new_x = self.body(self.embedding(x, self.train_step))
-        if type(new_x) == tuple:
-            new_x, deform_rot = new_x
-        dx = (new_x - x).detach()
-        return [new_x, deform_rot]
 
 class DeformationGraph(nn.Module):
     def __init__(self, aabb=None, dg_dir = None, **kwargs): # dg_dir: embedded deformation graph directory
@@ -81,99 +67,16 @@ class DeformationGraph(nn.Module):
         x = x.reshape(*original_shape)
         return x
 
-    def forward(self, inputs): # inputs: [B, 3 + n_positional_encoding]
+    def forward(self, inputs): # inputs: [B, n_point_dim(=3) + n_positional_encoding]
         assert len(inputs.shape) == 2
 
         pts, positional_encoding = inputs[:, :3], inputs[:, 3:].float()
-        pts_original = to_original(self.aabb, pts)
 
         min_dis, min_id = argmin_dis_batch(pts_original, self.vd, self.K)
         pts_deformed, R = self.deform_forward_K(min_dis, min_id, pts_original)
 
-        pts_deformed = to_contract(self.aabb, pts_deformed)
         
         pts_deformed[:, 0][min_dis[:, 0] > 2.1e-4] = 1e9 # if the distance to the EDG is farther than the threshold, we map it to a very far place (1e9), that will get an empty occupancy in outside codes. 
         
         return (pts_deformed, (R, self.deform_direction)) # deform_direction is used in outside code.
-
-class Embedder:
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
-        self.create_embedding_fn()
-
-    def create_embedding_fn(self):
-        embed_fns = []
-        d = self.kwargs["input_dims"]
-        self.increasing_coeff = self.kwargs["increasing_coeff"]
-        self.max_increasing = self.kwargs["max_increasing"]
-        out_dim = 0
-        if self.kwargs["include_input"]:
-            embed_fns.append(lambda x: x)
-            out_dim += d
-
-        max_freq = self.kwargs["max_freq_log2"]
-        N_freqs = self.kwargs["num_freqs"]
-        self.N_freqs = N_freqs
-
-        if self.kwargs["log_sampling"]:
-            freq_bands = 2.0 ** torch.linspace(0.0, max_freq, steps=N_freqs)
-        else:
-            freq_bands = torch.linspace(2.0**0.0, 2.0**max_freq, steps=N_freqs)  #  * 0.5
-
-        for freq in freq_bands:
-            for p_fn in self.kwargs["periodic_fns"]:
-                embed_fns.append(lambda x, p_fn=p_fn, freq=freq: p_fn(x * freq))
-                out_dim += d
-
-        self.embed_fns = embed_fns
-        self.out_dim = out_dim
-
-    def embed(self, inputs, step=None):
-        if step is None or self.increasing_coeff is None:
-            alpha = torch.ones((self.N_freqs,)).numpy()
-        else:
-            if self.max_increasing is not None and step > self.max_increasing:
-                step = self.max_increasing
-            alpha = torch.clamp(step * self.increasing_coeff - torch.arange(0, self.N_freqs), 0, 1).numpy()
-        # print("alpha", alpha)
-        if not self.kwargs["return_alpha"]:
-            res = []
-            for i, fn in enumerate(self.embed_fns):
-                id = (i - 1) // 2
-                if id == -1:
-                    res.append(fn(inputs))
-                else:
-                    res.append(fn(inputs) * alpha[id])
-            res = torch.cat(res, -1)
-            return res
-        else:
-            res = []
-            for i, fn in enumerate(self.embed_fns):
-                res.append(fn(inputs))
-            res = torch.cat(res, -1)
-            return res, alpha
-
-
-def get_embedder(
-    multires, input_dims, increasing_coeff=None, max_freq_log2=None, return_alpha=False, max_increasing=None
-):
-    if multires == 0:
-        return nn.Identity(), input_dims
-    if max_freq_log2 is None:
-        max_freq_log2 = multires - 1
-    embed_kwargs = {
-        "include_input": True,
-        "input_dims": input_dims,
-        "max_freq_log2": max_freq_log2,
-        "num_freqs": multires,
-        "log_sampling": True,
-        "periodic_fns": [torch.sin, torch.cos],
-        "increasing_coeff": increasing_coeff,
-        "max_increasing": max_increasing,
-        "return_alpha": return_alpha,
-    }
-
-    embedder_obj = Embedder(**embed_kwargs)
-    embed = lambda x, step=None, eo=embedder_obj: eo.embed(x, step)
-    return embed, embedder_obj.out_dim, input_dims, multires
 
